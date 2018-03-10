0c6e323adde720b02f11dc62fce08650bfa9eaec
crisalfamidia <37242175+crisalfamidia@users.noreply.github.com>
2018-03-10 10:05:58 -0300

    teste01
    01

---
+++ ContactsCrudMvc.Web/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+
+namespace ContactsCrudMvc.Web.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        // [Display(Name ="Quem?")]
+        public String Nome { get; set; }
+        
+        [Display(Name = "Telefone")]
+        public String Fone { get; set; }
+        [Display(Name = "Endereço de E-Mail")]
+        public String Email { get; set; }
+        
+
+    }
+}
