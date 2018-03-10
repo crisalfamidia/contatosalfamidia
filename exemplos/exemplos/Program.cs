8edf9921cf1520fffaffbd9bc1856931ef4c38a3
crisalfamidia <37242175+crisalfamidia@users.noreply.github.com>
2018-03-10 11:22:18 -0300

    comand
    .

---
+++ exemplos/exemplos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inicio
+            Console.WriteLine("Meu aplicativo\n\n");
+            var idade = 18;
+            var nomePessoa = "João ";
+            String sobrenome = "da Silva";
+
+            String nomeMae;
+            nomeMae = "Joana";
+            int idadeMae = 54;
+
+
+            String NomeCompleto(String primeiro, String segundo)
+            {
+                return primeiro + segundo;
+            }
+
+            void CalculoIdade(int idade1, int idade2)
+            {
+                int idadetotal = idade1 + idade2;
+                Console.WriteLine("\n\nIdade Filho + idade da Mãe: " + idadetotal);
+
+            }
+            Console.WriteLine("Nome: " + nomePessoa + " " + sobrenome+"\n" + "Idade: " + idade);
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine("Teste...\nIdade: " + idade.ToString());
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine("Nome da mãe: "+nomeMae);
+            Console.WriteLine("\nIdade da mãe: " + idadeMae);
+            Console.WriteLine("\n\n\n");
+
+            Console.WriteLine("Nome Completo: "+ NomeCompleto(nomePessoa, sobrenome));
+            Console.WriteLine("\n\n\n");
+            CalculoIdade(idade,idadeMae);
+            Console.ReadKey();
+        }
+    }
+}
